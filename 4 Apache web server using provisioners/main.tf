7cac5b99c5f7ac90f36b48c167e443c99a227ef3
Paras Bhayani <37619422+parasbhayani@users.noreply.github.com>
2022-03-11 20:49:24 +0530

    Update main.tf

--- 4 Apache web server using provisioners/main.tf
+++ 4 Apache web server using provisioners/main.tf
@@ -14,8 +14,7 @@ resource "aws_instance" "webserver" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
-      "sudo mv index.html /var/www/html/"
+      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > /var/www/html/index.html",
     ]
     connection {
       type        = "ssh"
